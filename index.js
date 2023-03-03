c4eb24c2f046dbd143139a1fb1c3aad0f73d9c86
adegbite faruq <adegbitefaruq5@gmail.com>
2023-03-03 21:07:08 +0100

    push it

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+{/* 
+<!-- <div class="art">
+        <div class="art1">
+            <h3 class="font">ARTbyAI</h3>
+            <a href=""><P class="font1">Gallery</P></a>
+            <p class="font1">About us</p>
+            <p class="font2">Contact us</p>
+        </div>
+    </div> -->
